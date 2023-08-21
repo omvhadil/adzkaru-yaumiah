fb05772a47edebf051f3140ab9abc93a0acec99b
omvhadil <fadil.rohman.327@gmail.com>
2023-08-21 08:12:17 +0700

    new font

--- src/views/Dzikir.vue
+++ src/views/Dzikir.vue
@@ -54,7 +54,7 @@ const jmlSubname = dzikri[categoryNumber.value].subName.length
             <td class="col-11" :class="item.arab ? 'border-bottom' : ''">
               <h2
                 style="direction: rtl; unicode-bidi: bidi-override; line-height: 2 !important"
-                class="m-0 py-1"
+                class="m-0 py-1 fw-normal"
                 align="justify"
               >
                 {{ item.arab }}
