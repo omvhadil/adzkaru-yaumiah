f4691c50e0838081669ad6355fb7398b30a54b97
omvhadil <fadil.rohman.327@gmail.com>
2023-08-22 11:40:39 +0700

    slide

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueSplide from '@splidejs/vue-splide';
 
 import './assets/main.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -8,6 +9,8 @@ import 'bootstrap/dist/js/bootstrap.min.js'
 import 'remixicon/fonts/remixicon.css'
 
 const app = createApp(App)
+
+app.use( VueSplide );
 
 app.use(router)
 
